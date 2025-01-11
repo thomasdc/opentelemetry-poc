9de4e8b9a03a969700c62e22703d1b38472b7210
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2025-01-11 11:16:23 +0100

    tracing

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
+using System.Diagnostics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// https://github.com/open-telemetry/opentelemetry-dotnet
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource
+        .AddService("OpenTelemetryPoc")
+        .AddAttributes(new List<KeyValuePair<string, object>>
+        {
+            new("Startup", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")),
+            new("AppVersion", "0.1.42")
+        }))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter());
 
 var app = builder.Build();
 
@@ -19,7 +36,20 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.Use((context, next) =>
+{
+    try
+    {
+        return next(context);
+    }
+    catch (Exception e)
+    {
+        Activity.Current?.AddException(e);
+        throw;
+    }
+});
+
+app.MapGet("/weatherforecast", (bool shouldError = false) =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
@@ -29,6 +59,13 @@ app.MapGet("/weatherforecast", () =>
                     summaries[Random.Shared.Next(summaries.Length)]
                 ))
             .ToArray();
+        Activity.Current?.AddEvent(new ActivityEvent("Weather forecast requested"));
+        if (shouldError)
+        {
+            var x = 1 / int.Parse("0");
+        }
+        
+        Activity.Current?.SetStatus(ActivityStatusCode.Ok);
         return forecast;
     })
     .WithName("GetWeatherForecast");
