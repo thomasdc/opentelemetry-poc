886a0bc014fce708250eb0207dda482816b8be0a
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2025-01-12 09:45:10 +0100

    efcore instrumentation + custom spans

---
+++ AuditContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OpenTelemetryPoc;
+
+public class AuditContext : DbContext
+{
+    public DbSet<AuditEntry> AuditEntries { get; set; }
+    
+    public string DbPath { get; }
+    
+    public AuditContext()
+    {
+        // https://learn.microsoft.com/nl-nl/ef/core/get-started/overview/first-app?tabs=netcore-cli#create-the-database
+        var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        DbPath = Path.Join(path, "blogging.db");
+    }
+    
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class AuditEntry
+{
+    public long Id { get; private set; }
+    public string RawUrl { get; set; }
+    public string Method { get; set; }
+    public string IpAddress { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using OpenTelemetryPoc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AuditContext>();
+
+builder.Services.AddSingleton(new DiagnosticsConfig());
 
 // https://github.com/open-telemetry/opentelemetry-dotnet
 builder.Services.AddOpenTelemetry()
@@ -20,7 +26,9 @@ builder.Services.AddOpenTelemetry()
             new("AppVersion", "0.1.42")
         }))
     .WithTracing(tracing => tracing
+        .AddSource(DiagnosticsConfig.SourceName)
         .AddAspNetCoreInstrumentation()
+        .AddEntityFrameworkCoreInstrumentation()
         .AddOtlpExporter())
     .WithLogging(logging => logging
         .AddOtlpExporter());
@@ -53,7 +61,12 @@ app.Use((context, next) =>
     }
 });
 
-app.MapGet("/weatherforecast", ([FromServices] ILogger<Program> logger, bool shouldError = false) =>
+app.MapGet("/weatherforecast", async (
+        [FromServices] ILogger<Program> logger, 
+        [FromServices] AuditContext auditContext, 
+        [FromServices] DiagnosticsConfig diagnosticsConfig,
+        HttpContext httpContext,
+        bool shouldError = false) =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
@@ -69,9 +82,23 @@ app.MapGet("/weatherforecast", ([FromServices] ILogger<Program> logger, bool sho
         {
             var x = 1 / int.Parse("0");
         }
+
+        using (var span = diagnosticsConfig.Source.StartActivity("Audit logging"))
+        {
+            logger.LogInformation("Saving to audit log...");
+            var auditEntry = new AuditEntry
+            {
+                RawUrl = httpContext.Request.GetEncodedPathAndQuery(),
+                IpAddress = httpContext.Connection.RemoteIpAddress?.ToString() ?? "(unknown)",
+                Method = httpContext.Request.Method
+            };
+            await auditContext.AddAsync(auditEntry);
+            await auditContext.SaveChangesAsync();
+            span?.SetTag(DiagnosticsNames.AuditEntryId, auditEntry.Id);
+        }
         
         Activity.Current?.SetStatus(ActivityStatusCode.Ok);
-        return forecast;
+        return TypedResults.Ok(forecast);
     })
     .WithName("GetWeatherForecast");
 
@@ -86,4 +113,15 @@ internal static partial class LoggerExtensions
 {
     [LoggerMessage(LogLevel.Information, "Weather forecast requested (as log event) should error: `{ShouldError}`")]
     public static partial void WeatherForecastRequested(this ILogger logger, bool shouldError);
+}
+
+public class DiagnosticsConfig
+{
+    public const string SourceName = "audit-source";
+    public ActivitySource Source = new(SourceName);
+}
+
+public static class DiagnosticsNames
+{
+    public const string AuditEntryId = "audit.entry.id";
 }
